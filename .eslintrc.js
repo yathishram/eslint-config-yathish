b831c9fdbf3c83d4354232feb79d2a977e06bfb7
yathishram <123yathish.r@gmail.com>
2020-07-21 13:32:05 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,79 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['react-hooks', 'prettier'],
+  rules: {
+    'import/extensions': 'off',
+    'import/no-dynamic-require': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'error',
+    'newline-per-chained-call': 'off',
+    'jsx-a11y/label-has-for': [
+      'warn',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'no-confusing-arrow': 'off',
+    'no-use-before-define': 'off',
+    'prefer-template': 'error',
+    'require-yield': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-no-target-blank': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: false,
+        shorthandLast: true,
+        ignoreCase: true,
+        noSortAlphabetically: false,
+      },
+    ],
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 80,
+      },
+    ],
+    'react/require-extension': 'off',
+    'react/sort-prop-types': [
+      'warn',
+      {
+        callbacksLast: true,
+        ignoreCase: true,
+        requiredFirst: false,
+        sortShapeProp: true,
+      },
+    ],
+  },
+};
